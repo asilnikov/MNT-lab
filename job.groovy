d2c1042ac6658c08e87bf097e8cb63caf899d2db
asilnikov <51442050+asilnikov@users.noreply.github.com>
2019-09-22 22:41:09 +0300

    Update job.groovy

--- job.groovy
+++ job.groovy
@@ -53,7 +53,7 @@ for (i in (1..4)) {
         }
 
         steps {
-            shell('chmod +x ./script.sh; ./script.sh > output.txt; tar -czf ${BRANCH_NAME}_dsl_script.tar.gz job.groovy')
+            shell('chmod +x script.sh; ./script.sh > output.txt; tar -czf ${BRANCH_NAME}_dsl_script.tar.gz job.groovy')
                }
         publishers {
             archiveArtifacts {
