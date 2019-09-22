6023c1a2ce1ac7b35155fffbc59d06efeef773f3
asilnikov <asilnikov@inbox.ru>
2019-09-22 21:22:17 +0300

    dsl scrip groovy for task: dsl-plugin

---
+++ job.groovy
@@ -0,0 +1,71 @@
+def giturl = 'https://github.com/asilnikov/MNT-lab.git'
+job ("MNTLAB-silnikov-main-build-job") {
+
+    parameters{
+        choiceParam('BRANCH_NAME', ['asilnikov', 'master'], 'Branch name')
+        activeChoiceParam('steps_job') {
+            description('Choose job')
+            choiceType('CHECKBOX')
+            filterable(false)
+            groovyScript {
+                script('''return ["MNTLAB-asilnikov-child1-build-job", "MNTLAB-asilnikov-child2-build-job", "MNTLAB-asilnikov-child3-build-job", "MNTLAB-asilnikov-child4-build-job"]''')
+            }
+        }
+    }
+
+    steps {
+        downstreamParameterized {
+            trigger('$steps_job') {
+                block {
+                    buildStepFailure('FAILURE')
+                    failure('FAILURE')
+                    unstable('UNSTABLE')
+                }
+                parameters {
+                    predefinedProp('BRANCH_NAME', '$BRANCH_NAME')
+                }
+            }
+        }
+    }
+}
+
+for (i in (1..4)) {
+    job("MNTLAB-asilnikov-child${i}-build-job") {
+
+        scm {
+            git {
+                remote {
+                    url(giturl)
+                }
+                branch('$BRANCH_NAME')
+            }
+        }
+
+        parameters {
+        	activeChoiceParam('BRANCH_NAME') {
+                description('Choose branch')
+                choiceType('SINGLE_SELECT')
+                groovyScript {
+                    script('''("git ls-remote -h https://github.com/asilnikov/MNT-lab.git").execute().text.readLines().collect {
+                      it.split()[1].replaceAll(\'refs/heads/\', \'\')}.sort()''')
+                }
+            }
+        }
+
+        steps {
+            shell('''
+   bash script.sh > output.txt
+   tar -czf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy''')
+        }
+        publishers {
+            archiveArtifacts {
+                pattern('${BRANCH_NAME}_dsl_script.tar.gz')
+                allowEmpty(false)
+                onlyIfSuccessful(false)
+                fingerprint(false)
+                defaultExcludes(true)
+            }
+        }
+    }
+}
+
